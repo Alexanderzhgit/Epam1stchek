b453405b1270ecfd7fc1e13e079e9d4935816bca
Alexander Zh. <sashka.zhdanov@gmail.com>
2017-10-22 17:52:03 +0400

    Create class sweet

---
+++ Epam1stchekpoint/Epam1stchekpoint/Classes/Sweet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam1stchekpoint.Classes
+{
+    class Sweet
+    {
+        public string type { get; private set; }
+        public int sugar { get; private set; }
+        public int weight { get; private set; }
+
+        public Sweet(string sweetType, int sweetSugar, int sweetWeight)
+        {
+            this.type = sweetType;
+            this.sugar = sweetSugar;
+            this.weight = sweetWeight;
+        }
+    }
+}

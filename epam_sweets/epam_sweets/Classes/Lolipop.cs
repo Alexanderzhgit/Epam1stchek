d133bfcfb9521ae2e21c3f393e0cdecddbd6def4
Alexander Zh. <sashka.zhdanov@gmail.com>
2017-10-29 02:14:47 +0400

    Create class Lolipop

---
+++ epam_sweets/epam_sweets/Classes/Lolipop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam_sweets.Classes
+{
+    class Lolipop : Sweets
+    {
+        private int LolipopSugar;
+        public Lolipop (string _type, string _name, int _weight, float _sugar);
+
+                        
+    }
+}
--- epam_sweets/epam_sweets/Classes/Sweets.cs
+++ epam_sweets/epam_sweets/Classes/Sweets.cs
@@ -6,26 +6,27 @@ using System.Threading.Tasks;
 
 namespace epam_sweets
 {
-    public class Sweets
+    public abstract class Sweets
     {
-        public string Type;
-        public string Name;
-        public int Weight;
-        public float Sugar;
+        public string Typeof { get; private set; }
+        public string Name { get; private set; }
+        public int Weight { get; private set; }
+        public float Sugar { get; private set; }
         public static int sweetcount = 0;
 
-        public Sweets () 
+        public Sweets ( string _type, string _name, int _weight, float _sugar) 
             {
-                Type = "Lolipop";
-                Name = "FlashSweet";
-                Weight = 5;
-                Sugar = 0.2f;
+            Typeof = _type;
+            Name = _name;
+            Weight = _weight;
+            Sugar = _sugar;
+
                 sweetcount++;
                 Print();
             }
         public void Print ()
         {
-            Console.WriteLine("Type of Sweet: " + Type);
+            Console.WriteLine("Type of Sweet: " + Typeof);
             Console.WriteLine("Name: " + Name);
             Console.WriteLine("Weight: " + Weight);
             Console.WriteLine("Sugar amount: " + Sugar);
